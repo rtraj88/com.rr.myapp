8985fb7a03dc2f9d297f84e65532a17515800977
srinivas kanegave <srinu.dd2012@gmail.com>
2017-10-14 21:35:34 -0500

    initial commit

---
+++ src/main/java/com/rr/apps/MyApplication.java
@@ -0,0 +1,13 @@
+package com.rr.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication extends SpringApplication{
+	
+	 public static void main(String[] args) throws Exception {
+	        SpringApplication.run(MyApplication.class, args);
+	    }
+
+}
---
+++ src/main/java/com/rr/apps/controller/MyController.java
@@ -0,0 +1,16 @@
+package com.rr.apps.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Hello welcome to spring boot application";
+	}
+	
+
+}
